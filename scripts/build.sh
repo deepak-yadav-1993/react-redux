cf80be01cedab319b4cfd9d31f227ff7f98dfeab
Deepak Yadav <deepak.yadav@ericsson.com>
2020-05-20 18:29:08 -0400

    Added pipeline to push to docker

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Building the"
+docker build -t personal_finance_app:latest .
+docker push deepaky193/personal_finance_app:latest
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,7 +5,8 @@ export const ConfigureStore = () => {
   const store = createStore(
     combineReducers({
       loginState: authStore,
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return store;
