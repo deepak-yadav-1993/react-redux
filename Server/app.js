42f1fd2aad7c2edefdcdb5db17e21a68a40494e6
Deepak Yadav <deepakYadav_personalMacBook>
2021-03-28 19:24:55 -0400

    Updated prettier config and started using dotenv module

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-const endPoint = require("./endpoints");
-const getRoutes = require("./src/routes/getRoutes");
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger_output.json");
+const endPoint = require('./endpoints');
+const getRoutes = require('./src/routes/getRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
 
 const port = process.env.PORT;
 const dbPassword = process.env.DBPWD;
@@ -20,11 +20,11 @@ const connectToDb = async () => {
     app.listen(port, () => {
       console.log(`Listening on port ${port}`);
     });
-    app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
     endPoint(app);
     getRoutes(app);
   } catch (err) {
-    console.trace("Error Message\n", err?.message, "\nErr\n", err);
+    console.trace('Error Message\n', err?.message, '\nErr\n', err);
   }
 };
 
--- Server/src/routes/getRoutes.js
+++ Server/src/routes/getRoutes.js
@@ -1,16 +1,16 @@
-const response = require("./../common");
+const response = require('./../common');
 
 const healthResponse = {
   ...response,
-  status: "ok",
-  message: "The database service is up and running",
+  status: 'ok',
+  message: 'The database service is up and running',
 };
 
 module.exports = (app) => {
-  app.get("/api", (req, res) => {
+  app.get('/api', (req, res) => {
     res.send(healthResponse);
   });
-  app.get("*", (req, res) => {
+  app.get('*', (req, res) => {
     //The default route
     res.send(response);
   });
