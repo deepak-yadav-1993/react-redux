e83739a8ef1c794f0985a40d13f904fc1eb836b1
Deepak Yadav <deepak.yadav@lightspeedhq.com>
2022-11-14 18:45:14 +0530

    change index

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
