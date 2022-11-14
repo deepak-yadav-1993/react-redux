3d3098f346b2ce5c50f99866d1fa564d7b1ce2a2
Deepak Yadav <41962013+deepak-yadav-1993@users.noreply.github.com>
2022-11-14 18:10:37 +0530

    Update retrigger logic (#10)
    Co-authored-by: Deepak Yadav <deepak.yadav@lightspeedhq.com>

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
